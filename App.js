4c1d97975582280441e5c459d5a6f21ba2648e1a
sgb004 <sgb004@gmail.com>
2022-07-17 02:27:33 -0500

    Changes to use controllers of Providers and News

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import { rsstojsonConvert } from './utils/rss-to-json';
 import Constants from 'expo-constants';
 import * as SQLite from 'expo-sqlite';
 import DB_SQLite from './utils/db-sqlite';
+import ProvidersController from './controllers/providers-controller';
+import NewsController from './controllers/news-controller';
 
-const db = new DB_SQLite('data07.db');
+const db = new DB_SQLite('data18.db');
 
 const styles = StyleSheet.create({
 	container: {
@@ -20,48 +22,35 @@ export default function App() {
 	let [listItems, setListItems] = useState([]);
 
 	useEffect(() => {
-		db.execute(
-			`CREATE TABLE IF NOT EXISTS news(
-				id INTEGER PRIMARY KEY NOT NULL, 
-				guid TEXT,
-				title TEXT,
-				pubDate TEXT,
-				link TEXT,
-				author TEXT,
-				thumbnail TEXT,
-				read BOOL,
-				bookmark BOOL
-			);`
-		);
+		const providersController = new ProvidersController(db);
+		const newsController = new NewsController(db);
 
 		fetch('http://feeds.weblogssl.com/xatakamx')
 			.then((response) => response.text())
 			.then((data) => {
 				data = rsstojsonConvert(data);
-				data.items.forEach((item) => {
-					console.log(item);
-					db.execute(
-						'INSERT INTO news (guid, title, pubDate, link, author, thumbnail, read, bookmark) values (?, ?, ?, ?, ?, ?, 0, 0)',
-						[
-							item.guid,
-							item.title,
-							item.pubDate,
-							item.link,
-							item.author,
-							item.thumbnail,
-						]
-					);
-				});
-				setListItems(data.items);
+
+				data.url = 'http://feeds.weblogssl.com/xatakamx';
+				providersController
+					.register(data)
+					.then((providerId) => {
+						console.log({ providerId });
+
+						data.items.map((item) => {
+							item.providerId = providerId;
+							return item;
+						});
+
+						return newsController.register(data.items);
+					})
+					.then(() => newsController.getAll())
+					.then((news) => {
+						//console.log(news);
+						setListItems(news);
+					})
+					.catch((error) => console.log(error));
 			});
 	}, []);
-
-	db.select(
-		'SELECT * FROM news',
-		[],
-		(rows) => console.log('DEVUELTO 8', JSON.stringify(rows)),
-		(error) => console.log(error)
-	);
 
 	return (
 		<View style={styles.container}>
