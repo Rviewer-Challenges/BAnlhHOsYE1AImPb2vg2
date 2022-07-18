f3f610192544403c434e8cd2e689a189b9cdb687
sgb004 <sgb004@gmail.com>
2022-07-18 15:25:07 -0500

    Changes to use downloader-rss

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import { useState, useEffect } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import ListItem from './components/list-item/list-item';
-import { rsstojsonConvert } from './utils/rss-to-json';
 import Constants from 'expo-constants';
-import * as SQLite from 'expo-sqlite';
 import DB_SQLite from './utils/db-sqlite';
-import ProvidersController from './controllers/providers-controller';
-import NewsController from './controllers/news-controller';
+import downloaderRSS from './utils/downloader-rss';
 
 const db = new DB_SQLite('data18.db');
 
@@ -22,34 +19,12 @@ export default function App() {
 	let [listItems, setListItems] = useState([]);
 
 	useEffect(() => {
-		const providersController = new ProvidersController(db);
-		const newsController = new NewsController(db);
-
-		fetch('http://feeds.weblogssl.com/xatakamx')
-			.then((response) => response.text())
-			.then((data) => {
-				data = rsstojsonConvert(data);
-
-				data.url = 'http://feeds.weblogssl.com/xatakamx';
-				providersController
-					.register(data)
-					.then((providerId) => {
-						console.log({ providerId });
-
-						data.items.map((item) => {
-							item.providerId = providerId;
-							return item;
-						});
-
-						return newsController.register(data.items);
-					})
-					.then(() => newsController.getAll())
-					.then((news) => {
-						//console.log(news);
-						setListItems(news);
-					})
-					.catch((error) => console.log(error));
-			});
+		downloaderRSS(db)
+			.then((news) => {
+				console.log('TODAS LAS NOTICIAS');
+				setListItems(news);
+			})
+			.catch((error) => console.log(error));
 	}, []);
 
 	return (
