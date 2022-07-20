433f39dd837ced3586809c714262b3ef37f1fed2
sgb004 <sgb004@gmail.com>
2022-07-19 21:17:23 -0500

    Styles and structure for when there is no thumbnail

--- components/list-item/list-item.js
+++ components/list-item/list-item.js
@@ -8,8 +8,7 @@ const styles = StyleSheet.create({
 		paddingBottom: 10,
 		paddingLeft: 10,
 	},
-	content: {
-		flex: 1,
+	row: {
 		flexDirection: 'row',
 	},
 	img: {
@@ -32,22 +31,39 @@ const styles = StyleSheet.create({
 	footer: {
 		fontSize: 13,
 	},
+	footerRight: {
+		width: Dimensions.get('window').width - 140,
+		marginTop: 2,
+		marginLeft: 10,
+	},
 });
 
 const ListItem = ({ title, thumbnail, pubDate, provider }) => {
+	thumbnail = thumbnail === '' ? provider.image : thumbnail;
 	pubDate = formatDate(pubDate);
+
 	return (
 		<View style={styles.container}>
-			<View style={styles.content}>
-				<View>
-					{thumbnail != '' && <Image style={styles.img} source={{ uri: thumbnail }} />}
-					<Text style={styles.date}>{pubDate}</Text>
-				</View>
-				<View style={styles.text}>
+			{thumbnail === '' ? (
+				<>
 					<Text style={styles.title}>{title}</Text>
-					<Text style={styles.footer}>{provider.title}</Text>
+					<View style={styles.row}>
+						<Text style={styles.date}>{pubDate}</Text>
+						<Text style={[styles.footer, styles.footerRight]}>{provider.title}</Text>
+					</View>
+				</>
+			) : (
+				<View style={styles.row}>
+					<View>
+						<Image style={styles.img} source={{ uri: thumbnail }} />
+						<Text style={styles.date}>{pubDate}</Text>
+					</View>
+					<View style={styles.text}>
+						<Text style={styles.title}>{title}</Text>
+						<Text style={styles.footer}>{provider.title}</Text>
+					</View>
 				</View>
-			</View>
+			)}
 		</View>
 	);
 };
