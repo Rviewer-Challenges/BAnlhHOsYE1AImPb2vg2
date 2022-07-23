2ef830bf63f120d524803168ba0c0f27aba05ffa
sgb004 <sgb004@gmail.com>
2022-07-23 01:38:54 -0500

    Added swipe to left and right to items view

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { FlatList, StyleSheet, View, PanResponder } from 'react-native';
 import ListItem from './components/list-item/list-item';
 import Constants from 'expo-constants';
 import DB_SQLite from './utils/db-sqlite';
@@ -7,7 +7,7 @@ import downloaderRSS from './utils/downloader-rss';
 import { getTimeFromDate } from './utils/custom-date';
 import NewsController from './controllers/news-controller';
 
-const db = new DB_SQLite('data19.db');
+const db = new DB_SQLite('data22.db');
 
 const styles = StyleSheet.create({
 	container: {
@@ -23,37 +23,75 @@ export default function App() {
 
 		downloaderRSS(db)
 			.then((news) => {
-			  console.log('Obteniendo automaticamente')
-			  
+				console.log('Obteniendo automaticamente');
+
 				setListItems(news);
-				
+
 				timerDownloadRSS = setTimeout(() => {
 					downloadRSS();
 				}, 60000);
-				
 			})
 			.catch((error) => console.log(error));
 	};
+
+	let y = 0;
+	let x = 0;
+	let isScrolling = false;
 	let timerDownloadRSS;
 
 	let [listItems, setListItems] = useState([]);
+	let list = useRef(null);
 
 	useEffect(() => {
-	  const newsController = new NewsController(db);
+		const newsController = new NewsController(db);
 
 		newsController
 			.getAll()
 			.then((news) => {
-			  console.log('Obteniendo al inicio');
-			  setListItems(news);
-			  downloadRSS();
+				console.log('Obteniendo al inicio');
+				setListItems(news);
+				//downloadRSS();
 			})
 			.catch((error) => console.log(error));
 	}, []);
 
 	return (
 		<View style={styles.container}>
-			<FlatList data={listItems} renderItem={({ item }) => <ListItem {...item} />} />
+			<FlatList
+				ref={list}
+				data={listItems}
+				renderItem={({ item }) => <ListItem {...item} />}
+				onTouchStart={(evt) => {
+					x = evt.nativeEvent.pageX;
+					y = evt.nativeEvent.pageY;
+				}}
+				onTouchMove={(evt) => {
+					let _x = Math.abs(x - evt.nativeEvent.pageX);
+					let _y = Math.abs(y - evt.nativeEvent.pageY);
+					if (_x > 5 && _y < 5 && !isScrolling) {
+						list.current.setNativeProps({
+							scrollEnabled: false,
+						});
+					}
+					y = evt.nativeEvent.pageY;
+				}}
+				onTouchEnd={() =>
+					list.current.setNativeProps({
+						scrollEnabled: true,
+					})
+				}
+				onTouchEndCapture={() =>
+					list.current.setNativeProps({
+						scrollEnabled: true,
+					})
+				}
+				onScroll={() => {
+					isScrolling = true;
+				}}
+				onScrollEndDrag={() => {
+					isScrolling = false;
+				}}
+			/>
 		</View>
 	);
 }
--- components/list-item/list-item.js
+++ components/list-item/list-item.js
@@ -1,8 +1,11 @@
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import { useRef, useState } from 'react';
+import { View, Text, StyleSheet, Image, Dimensions, Animated, PanResponder } from 'react-native';
 import formatDate from '../../utils/custom-date';
 
+const windowWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
-	container: {
+	content: {
 		paddingTop: 10,
 		paddingRight: 10,
 		paddingBottom: 10,
@@ -18,52 +21,169 @@ const styles = StyleSheet.create({
 		marginTop: 7,
 	},
 	text: {
-		width: Dimensions.get('window').width - 130,
+		width: windowWidth - 130,
 		marginLeft: 10,
 	},
-	title: { fontSize: 19 },
+	title: {
+		fontSize: 19,
+		fontWeight: 'bold',
+		color: '#03071e',
+	},
+	titleRead: {
+		fontWeight: 'normal',
+		color: '#2f3e46',
+	},
 	date: {
 		width: 100,
 		textAlign: 'center',
 		fontSize: 13,
 		marginTop: 2,
+		color: '#03071e',
 	},
 	footer: {
 		fontSize: 13,
+		color: '#03071e',
 	},
 	footerRight: {
-		width: Dimensions.get('window').width - 140,
+		width: windowWidth - 140,
 		marginTop: 2,
 		marginLeft: 10,
 	},
+	icons: {
+		flexDirection: 'row',
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		//backgroundColor: '#f0f',
+		width: '100%',
+		height: '100%',
+		justifyContent: 'center',
+	},
+	icon: {
+		width: '50%',
+		height: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+		opacity: 0,
+	},
+	bookmark: {
+		backgroundColor: '#ffca3a',
+	},
+	swipeLeft: { backgroundColor: '#1982c4' },
 });
 
 const ListItem = ({ title, thumbnail, pubDate, provider }) => {
+	const pan = useRef(new Animated.ValueXY()).current;
+
+	const panResponder = useRef(
+		PanResponder.create({
+			onMoveShouldSetPanResponder: () => true,
+			onPanResponderMove: Animated.event([null, { dx: pan.x }], {
+				useNativeDriver: false,
+			}),
+		})
+	).current;
+
+	const restorePosition = () => {
+		if (pan.x !== 0) {
+			Animated.spring(pan, {
+				toValue: { x: 0, y: 0 },
+				useNativeDriver: false,
+			}).start();
+		}
+	};
+
+	const getPosition = () => {
+		let panX = parseFloat(JSON.stringify(pan.x));
+		return (panX * 100) / windowWidth;
+	};
+
+	const onTouchEnd = () => {
+		const position = getPosition();
+
+		if (position > 25) {
+			console.log('AGREGANDO A FAVORITOS');
+			status.isBookmark = !status.isBookmark;
+		} else if (position < -25) {
+			console.log('MARCAR COMO LEIDO');
+			status.isRead = !status.isRead;
+		}
+
+		setStatus({ isBookmark: status.isBookmark, isRead: status.isRead });
+	};
+
+	let [swipeDirection, setSwipeDirection] = useState('');
+	let [status, setStatus] = useState({ isBookmark: false, isRead: false });
+
+	let backgroundColor = 'transparent';
+	if (swipeDirection === 'right') {
+		backgroundColor = styles.bookmark.backgroundColor;
+	} else if (swipeDirection === 'left') {
+		backgroundColor = styles.swipeLeft.backgroundColor;
+	}
+
 	thumbnail = thumbnail === '' ? provider.image : thumbnail;
 	pubDate = formatDate(pubDate);
 
 	return (
-		<View style={styles.container}>
-			{thumbnail === '' ? (
-				<>
-					<Text style={styles.title}>{title}</Text>
-					<View style={styles.row}>
-						<Text style={styles.date}>{pubDate}</Text>
-						<Text style={[styles.footer, styles.footerRight]}>{provider.title}</Text>
-					</View>
-				</>
-			) : (
-				<View style={styles.row}>
-					<View>
-						<Image style={styles.img} source={{ uri: thumbnail }} />
-						<Text style={styles.date}>{pubDate}</Text>
-					</View>
-					<View style={styles.text}>
-						<Text style={styles.title}>{title}</Text>
-						<Text style={styles.footer}>{provider.title}</Text>
-					</View>
+		<View style={{ backgroundColor: backgroundColor }}>
+			<View style={styles.icons}>
+				<View style={[styles.icon, { opacity: swipeDirection === 'right' ? 100 : 0 }]}>
+					<Text>Fav</Text>
 				</View>
-			)}
+				<View style={[styles.icon, { opacity: swipeDirection === 'left' ? 100 : 0 }]}>
+					<Text>Lei</Text>
+				</View>
+			</View>
+			<Animated.View
+				style={{
+					transform: [{ translateX: pan.x }, { translateY: 0 }],
+					backgroundColor: status.isBookmark ? styles.bookmark.backgroundColor : '#fff',
+				}}
+				{...panResponder.panHandlers}
+				onTouchStart={() => restorePosition()}
+				onTouchMove={() => {
+					const position = getPosition();
+					if (position > 0) {
+						setSwipeDirection('right');
+					} else if (position < 0) {
+						setSwipeDirection('left');
+					}
+				}}
+				onTouchEnd={() => {
+					onTouchEnd();
+					restorePosition();
+				}}
+				onTouchCancel={() => {
+					onTouchEnd();
+					restorePosition();
+				}}
+			>
+				{thumbnail === '' ? (
+					<View style={[styles.content]}>
+						<Text style={styles.title}>{title}</Text>
+						<View style={styles.row}>
+							<Text style={styles.date}>{pubDate}</Text>
+							<Text style={[styles.footer, styles.footerRight]}>
+								{provider.title}
+							</Text>
+						</View>
+					</View>
+				) : (
+					<View style={[styles.content, styles.row]}>
+						<View>
+							<Image style={styles.img} source={{ uri: thumbnail }} />
+							<Text style={styles.date}>{pubDate}</Text>
+						</View>
+						<View style={styles.text}>
+							<Text style={[styles.title, status.isRead ? styles.titleRead : {}]}>
+								{title}
+							</Text>
+							<Text style={styles.footer}>{provider.title}</Text>
+						</View>
+					</View>
+				)}
+			</Animated.View>
 		</View>
 	);
 };
