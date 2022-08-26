0baeef651b6659296de2a36bbe01fea7b72ea5f9
sgb004 <sgb004@gmail.com>
2022-08-26 16:19:37 -0500

    When item is opened it is updated as read

--- components/item-screen/item-screen.js
+++ components/item-screen/item-screen.js
@@ -1,5 +1,6 @@
 import { Text, View, Image, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
+import NewsData from '../../utils/news-data';
 
 const styles = StyleSheet.create({
 	container: {
@@ -14,7 +15,13 @@ const ItemScreen = ({ route, navigation }) => {
 	const { id, title, thumbnail, pubDate, provider, bookmark, read } = route.params;
 
 	return (
-		<View style={styles.container}>
+		<View
+			style={styles.container}
+			onLayout={() => {
+				console.log('EN LAYOUT');
+				NewsData.newsItems.updateRead(id, true);
+			}}
+		>
 			<Image style={{ width: '100%', height: '40%' }} source={{ uri: thumbnail }} />
 			<Text>{title}</Text>
 			<Text>{pubDate}</Text>
