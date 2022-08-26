3d141343605905202da583a387aba73324d0fdb0
sgb004 <sgb004@gmail.com>
2022-08-26 16:20:13 -0500

    Refresh list bookmarks when is in focus

--- components/bookmarks-screen/bookmarks-screen.js
+++ components/bookmarks-screen/bookmarks-screen.js
@@ -1,14 +1,18 @@
 import ListItem from '../list-item/list-item';
 import CustomFlatListSwipe from '../custom-flatlist-swipe/custom-flatlist-swipe';
 import NewsData from '../../utils/news-data';
+import { useEffect, useState } from 'react';
 
 const BookmarksScreen = ({ navigation }) => {
-	return (
-		<CustomFlatListSwipe
-			data={NewsData.getBookmarks()}
-			renderItem={({ item }) => <ListItem {...item} />}
-		/>
-	);
+	let [news, setNews] = useState(NewsData.getBookmarks());
+	useEffect(() => {
+		const willFocus = navigation.addListener('focus', () => {
+			setNews(NewsData.getBookmarks());
+		});
+		return willFocus;
+	}, [navigation]);
+
+	return <CustomFlatListSwipe data={news} renderItem={({ item }) => <ListItem {...item} />} />;
 };
 
 export default BookmarksScreen;
