746f1c5f9d5a0ea5a400fc4bce26a66c7a43cae0
sgb004 <sgb004@gmail.com>
2022-09-09 00:51:56 -0500

    Using the item-content-webview component in item-screen

--- components/item-screen/item-screen.js
+++ components/item-screen/item-screen.js
@@ -1,22 +1,28 @@
-import { Text, View, Image, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Image, StyleSheet, ScrollView, Easing, Animated } from 'react-native';
 import NewsData from '../../utils/news-data';
+import NewsContent from '../../utils/news-content';
+import ItemContentWebView from '../item-content-webview/item-content-webview';
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		//paddingTop: Constants.statusBarHeight,
+		height: '100%',
 	},
 	content: {
 		paddingTop: 10,
 		paddingRight: 10,
 		paddingBottom: 10,
 		paddingLeft: 10,
+		height: '100%',
 	},
 	title: {
-		fontSize: 20,
+		fontSize: 22,
+		lineHeight: 24,
 		fontWeight: 'bold',
 		color: '#03071e',
+		width: '100%',
 	},
 	subtitleContainer: {
 		flexDirection: 'row',
@@ -29,27 +35,49 @@ const styles = StyleSheet.create({
 	},
 });
 
-StyleSheet;
-
 const ItemScreen = ({ route, navigation }) => {
 	const { id, title, thumbnail, pubDate, provider, bookmark, read } = route.params;
+
+	const [contentHTML, setContentHTML] = useState('');
+
+	const opacity = useRef(new Animated.Value(0)).current;
+	const opacityIn = () => {
+		Animated.timing(opacity, {
+			toValue: 1,
+			duration: 250,
+			easing: Easing.linear,
+			useNativeDriver: true,
+		}).start();
+	};
+
+	useEffect(() => {
+		const newsContent = new NewsContent();
+		newsContent
+			.getFileContent(id)
+			.then((data) => setContentHTML(data))
+			.catch((error) => console.log(error));
+	}, []);
 
 	return (
 		<View
 			style={styles.container}
-			onLayout={() => {
-				console.log('EN LAYOUT');
-				NewsData.newsItems.updateRead(id, true);
-			}}
+			onLayout={(event) => NewsData.newsItems.updateRead(id, true)}
 		>
-			<Image style={{ width: '100%', height: '40%' }} source={{ uri: thumbnail }} />
-			<View style={styles.content}>
-				<Text style={styles.title}>{title}</Text>
-				<View style={styles.subtitleContainer}>
-					<Text style={styles.subtitle}>{pubDate}</Text>
-					<Text style={styles.subtitle}>{provider.title}</Text>
-				</View>
-			</View>
+			<ScrollView>
+				<Animated.View style={{ opacity: opacity }}>
+					<View>
+						<Image style={{ width: '100%', height: 275 }} source={{ uri: thumbnail }} />
+						<View style={styles.content}>
+							<Text style={styles.title}>{title}</Text>
+							<View style={styles.subtitleContainer}>
+								<Text style={styles.subtitle}>{pubDate}</Text>
+								<Text style={styles.subtitle}>{provider.title}</Text>
+							</View>
+							<ItemContentWebView source={contentHTML} onReady={() => opacityIn()} />
+						</View>
+					</View>
+				</Animated.View>
+			</ScrollView>
 		</View>
 	);
 };
