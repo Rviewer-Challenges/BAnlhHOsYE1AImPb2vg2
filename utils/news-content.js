244ee59bc9112f21771fd8c6c291af09b41c0d14
sgb004 <sgb004@gmail.com>
2022-09-09 01:05:10 -0500

    cleanContent function added to prevent injection of javascript and css, this function also adds the ability to use tweets or use instagram embed and this function remove the thumbnail from the html code

--- utils/news-content.js
+++ utils/news-content.js
@@ -24,7 +24,7 @@ class NewsContent {
 		if (item.contentSaved == 1) {
 			this.saveAll(res);
 		} else {
-			this.saveContent(item.id, item.content)
+			this.saveContent(item.id, this.cleanContent(item.content, item.thumbnail))
 				.then(() => this.saveAll(res))
 				.catch((error) => rej(error));
 		}
@@ -39,6 +39,56 @@ class NewsContent {
 				.then(() => res())
 				.catch((error) => rej(error));
 		});
+	}
+
+	cleanContent(content, thumbnail = '') {
+		const attrs = ['href', 'src', 'alt', 'width', 'height', 'lang', 'dir'];
+		let socialMediaScripts = '';
+
+		if (thumbnail != '') {
+			let imgPosition = content.indexOf(thumbnail);
+
+			let tagIni = content.lastIndexOf('<', imgPosition);
+			let tagEnd = content.indexOf('>', imgPosition);
+			let tag = content.substring(tagIni, tagEnd + 1);
+			content = content.replace(tag, '');
+		}
+
+		content = content.replace(/<script/gm, '<!--<script');
+		content = content.replace(/<\/script>/gm, '</script>-->');
+		content = content.replace(/<style/gm, '<!--<style');
+		content = content.replace(/<\/style>/gm, '</style>-->');
+		content = content.replace(/<meta/gm, '<no-meta');
+		content = content.replace(/<link/gm, '<no-link');
+		content = content.replace(/src="\/\//gm, 'src="https://');
+		content = content.replace(/src='\/\//gm, "src='https://");
+		content = content.replace(/=/gm, '&#61;');
+		content = content.replace(
+			/<blockquote class&#61;"twitter-tweet">/gm,
+			'<blockquote class="twitter-tweet">'
+		);
+		content = content.replace(
+			/<blockquote class&#61;"instagram-media"/gm,
+			'<blockquote class="instagram-media"'
+		);
+
+		for (let i = 0; i < attrs.length; i++) {
+			content = content.replace(RegExp(attrs[i] + '&#61;', 'gm'), attrs[i] + '=');
+		}
+
+		//FOR TWITTER
+		if (content.indexOf('<blockquote class="twitter-tweet">') > -1) {
+			socialMediaScripts +=
+				'<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
+		}
+
+		//FOR INSTAGRAM
+		if (content.indexOf('<blockquote class="instagram-media"') > -1) {
+			socialMediaScripts +=
+				'<script async src="https://www.instagram.com/embed.js"></script>';
+		}
+
+		return content + socialMediaScripts;
 	}
 
 	saveContent(id, content) {
