1aeeb4a8ba24d85b52a263b0c879f6d73ab64d89
sgb004 <sgb004@gmail.com>
2022-09-21 14:24:20 -0500

    Thumbnail, title, publication date and provider was moved to webview to use a floating button that allows to visit the original news

--- components/item-screen/item-screen.js
+++ components/item-screen/item-screen.js
@@ -1,40 +1,8 @@
 import { useState, useEffect, useRef } from 'react';
-import { Text, View, Image, StyleSheet, ScrollView, Easing, Animated } from 'react-native';
+import { View, Easing, Animated } from 'react-native';
 import NewsData from '../../utils/news-data';
 import NewsContent from '../../utils/news-content';
 import ItemContentWebView from '../item-content-webview/item-content-webview';
-import ItemContentGoBtn from '../item-content-go-btn/item-content-go-btn';
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		//paddingTop: Constants.statusBarHeight,
-		height: '100%',
-	},
-	content: {
-		paddingTop: 10,
-		paddingRight: 10,
-		paddingBottom: 10,
-		paddingLeft: 10,
-		height: '100%',
-	},
-	title: {
-		fontSize: 22,
-		lineHeight: 24,
-		fontWeight: 'bold',
-		color: '#03071e',
-		width: '100%',
-	},
-	subtitleContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		marginTop: 5,
-	},
-	subtitle: {
-		fontSize: 14,
-		color: '#03071e',
-	},
-});
 
 const ItemScreen = ({ route, navigation }) => {
 	const { id, title, thumbnail, pubDate, provider, bookmark, read, link } = route.params;
@@ -61,30 +29,21 @@ const ItemScreen = ({ route, navigation }) => {
 
 	return (
 		<View
-			style={styles.container}
+			style={{
+				height: '100%',
+			}}
 			onLayout={(event) => NewsData.newsItems.updateRead(id, true)}
 		>
-			<ScrollView>
-				<Animated.View style={{ opacity: opacity }}>
-					<View
-						style={{
-							paddingBottom: 30,
-						}}
-					>
-						<Image style={{ width: '100%', height: 275 }} source={{ uri: thumbnail }} />
-						<View style={styles.content}>
-							<Text style={styles.title}>{title}</Text>
-							<View style={styles.subtitleContainer}>
-								<Text style={styles.subtitle}>{pubDate}</Text>
-								<Text style={styles.subtitle}>{provider.title}</Text>
-							</View>
-							<ItemContentWebView source={contentHTML} onReady={() => opacityIn()} />
-						</View>
-					</View>
-				</Animated.View>
-			</ScrollView>
-			<Animated.View style={{ opacity: opacity }}>
-				<ItemContentGoBtn link={link} />
+			<Animated.View style={{ opacity: opacity, height: '100%' }}>
+				<ItemContentWebView
+					title={title}
+					thumbnail={thumbnail}
+					pubDate={pubDate}
+					provider={provider}
+					link={link}
+					source={contentHTML}
+					onReady={() => opacityIn()}
+				/>
 			</Animated.View>
 		</View>
 	);
