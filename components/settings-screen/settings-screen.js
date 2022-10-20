954792189acd168c5611ca0a899cb4f56838b2b7
sgb004 <sgb004@gmail.com>
2022-10-20 13:48:54 -0500

    Changes to add component to select sources

--- components/settings-screen/settings-screen.js
+++ components/settings-screen/settings-screen.js
@@ -1,16 +1,26 @@
-import { View, ScrollView, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet, Text } from 'react-native';
 import Constants from 'expo-constants';
 import SettingsThemeOptions from './settings-theme-options';
+import SettingsSourcesOptions from './settings-sources-options';
 
 const styles = StyleSheet.create({
 	container: {
 		width: '100%',
 		height: '100%',
 	},
-	content: { paddingTop: Constants.statusBarHeight + 20, paddingRight: 10, paddingLeft: 10 },
+	content: { paddingRight: 10, paddingLeft: 10 },
+	header: {
+		fontSize: 22,
+		textAlign: 'center',
+		paddingTop: Constants.statusBarHeight + 15,
+		backgroundColor: '#588157',
+		color: '#fff',
+		paddingBottom: 15,
+	},
 	sectionTitle: {
 		fontSize: 20,
 		textAlign: 'center',
+		marginTop: 20,
 	},
 	controlTitle: {
 		fontSize: 18,
@@ -33,11 +43,22 @@ const styles = StyleSheet.create({
 const SettingsScreen = ({ navigation }) => {
 	return (
 		<View style={styles.container}>
+			<Text style={styles.header}>Configuración</Text>
 			<ScrollView style={styles.content}>
 				<SettingsThemeOptions
 					styles={styles}
 					onChange={(theme) => {
 						console.log('CHANGE THEME', theme);
+					}}
+				/>
+				<SettingsSourcesOptions
+					styles={styles}
+					sources={[
+						{ name: 'Xataka México', url: 'http://feeds.weblogssl.com/xatakamx', isActivated: false },
+						{ name: 'Arduino', url: 'https://blog.arduino.cc/feed', isActivated: true },
+					]}
+					onChange={(sources) => {
+						console.log({ sources });
 					}}
 				/>
 			</ScrollView>
