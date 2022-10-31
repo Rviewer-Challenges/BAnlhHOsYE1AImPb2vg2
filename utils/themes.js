fbf97364064abe62d5858af5bcad8c4eab044514
sgb004 <sgb004@gmail.com>
2022-10-31 14:47:25 -0600

    console.log was removed

--- utils/themes.js
+++ utils/themes.js
@@ -11,8 +11,6 @@ class Themes {
 	get(name) {
 		let styles = [this.#light[name]];
 
-		console.log('THEME', Themes.theme);
-
 		if (Themes.theme == 'dark') {
 			styles.push(this.#dark[name] != undefined ? this.#dark[name] : []);
 		}
