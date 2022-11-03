f000f67ec3c401a105e5bee39246ced13637f25c
sgb004 <sgb004@gmail.com>
2022-11-03 01:22:28 -0600

    Apply of changes of navigation bar

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import { useState, useEffect, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import NavigationBar from './components/navigation-bar/navigation-bar';
 import HomeScreen from './components/home-screen/home-screen';
 import BookmarksScreen from './components/bookmarks-screen/bookmarks-screen';
 import SettingsScreen from './components/settings-screen/settings-screen';
 import { Text, View, useColorScheme } from 'react-native';
-import NewsItems from './utils/news-items';
 import NewsData from './utils/news-data';
 import ItemScreen from './components/item-screen/item-screen';
 import Settings from './utils/settings';
 import DB_LOADED from './utils/db-sqlite-loaded';
 import Themes from './utils/themes';
+import CustomNavigationBar from './components/custom-navigation-bar/custom-navigation-bar';
 
 const horizontalAnimation = {
 	cardStyleInterpolator: ({ current, layouts }) => {
@@ -73,7 +72,7 @@ export default function App() {
 				/>
 				<Stack.Screen name="Item" component={ItemScreen} options={horizontalAnimation} />
 			</Stack.Navigator>
-			<NavigationBar />
+			<CustomNavigationBar />
 		</NavigationContainer>
 	) : (
 		<View>
