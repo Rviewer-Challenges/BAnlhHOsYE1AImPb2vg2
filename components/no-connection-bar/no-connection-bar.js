e5ada0458c3df22883d4be23a9df8ca238b9eac9
sgb004 <sgb004@gmail.com>
2022-11-04 20:22:56 -0600

    Bar to show that there is not internet connection.

---
+++ components/no-connection-bar/no-connection-bar.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Text, NativeEventEmitter, DeviceEventEmitter } from 'react-native';
+import Themes from '../../utils/themes';
+
+const NoConnectionBar = () => {
+	const [theme, changeTheme] = useState(Themes.theme);
+
+	useEffect(() => {
+		const eventEmitter = new NativeEventEmitter();
+		eventEmitter.listener = DeviceEventEmitter.addListener('CHANGE_THEME', () =>
+			changeTheme(Themes.theme)
+		);
+	}, []);
+
+	return (
+		<Text
+			style={{
+				textAlign: 'center',
+				backgroundColor: Themes.theme == 'dark' ? '#e63946' : '#ef233c',
+				color: '#fff',
+				paddingTop: 2,
+				paddingBottom: 2,
+			}}
+		>
+			Sin conexión
+		</Text>
+	);
+};
+
+export default NoConnectionBar;
